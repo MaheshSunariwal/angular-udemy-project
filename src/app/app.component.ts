36af455bc727a5ce88c5cad9246005a07fe45851
Mahesh Sunariwal <maheshkumar.sunariwal@icicibank.com>
2024-07-18 16:14:31 +0530

    modified the  tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,30 @@ import { RouterOutlet } from '@angular/router';
 import { HeadersComponent } from './headers/headers.component';
 import { UserComponent } from './user/user.component';
 import { dummy_users } from './users_data';
+import { TasksComponent } from './tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeadersComponent,UserComponent],
+  imports: [RouterOutlet,HeadersComponent,UserComponent,TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-demo-app';
   users = dummy_users ;
+  selecteduserid = 'u1' ;
 
-Onselectuser(id : string) {
-  console.log(id)
-}
+  Onselectuser(id : string) {
+    this.selecteduserid = id ;
+
+  }
+
+
+  get selecteduser () {
+    // console.log(this.users.find((user) => user.id === this.selecteduserid  ))
+    return this.users.find((user) => user.id === this.selecteduserid  )
+  }
+
+
 
 }
